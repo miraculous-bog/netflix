b4f7936c573e5c3376dbb5c69663d733b9d2eeb5
Bogdan <bogdan9110@gmail.com>
2023-06-02 16:51:32 +0300

    added frontend

--- main/src/common/components/Navbar/Navbar.tsx
+++ main/src/common/components/Navbar/Navbar.tsx
@@ -1,14 +1,14 @@
-// import { Search } from "@material-ui/icons";
-// import Search from '@mui/material/Search';
-import { useContext, useState } from "react";
+// import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+import { useState } from "react";
+import { Link } from 'react-router-dom';
 import "./navbar.scss";
-// import { Link } from "react-router-dom";
-// import { AuthContext } from "../../authContext/AuthContext";
-// import { logout } from "../../authContext/AuthActions";
 
 const Navbar = () => {
 	const [isScrolled, setIsScrolled] = useState(false);
-	//   const { dispatch } = useContext(AuthContext);
 
 	window.onscroll = () => {
 		setIsScrolled(window.pageYOffset === 0 ? false : true);
@@ -22,28 +22,29 @@ const Navbar = () => {
 						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
 						alt=""
 					/>
-					{/* <Link to="/" className="link"> */}
-					<span>Homepage</span>
-					{/* </Link> */}
-					{/* <Link to="/series" className="link"> */}
-					<span className="navbarmainLinks">Series</span>
-					{/* </Link> */}
-					{/* <Link to="/movies" className="link"> */}
-					<span className="navbarmainLinks">Movies</span>
-					{/* </Link> */}
+
+					<Link to="/" className="link">
+						<span>Homepage</span>
+					</Link>
+					<Link to="/series" className="link">
+						<span className="navbarmainLinks">Series</span>
+					</Link>
+					<Link to="/movies" className="link">
+						<span className="navbarmainLinks">Movies</span>
+					</Link>
 					<span>New and Popular</span>
 					<span>My List</span>
 				</div>
 				<div className="right">
-					{/* <Search className="icon" /> */}
+					<SavedSearchIcon className="icon" />
 					<span>KID</span>
-					{/* <Notifications className="icon" /> */}
+					<NotificationsActiveIcon className="icon" />
 					<img
 						src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 						alt=""
 					/>
 					<div className="profile">
-						{/* <ArrowDropDown className="icon" /> */}
+						<ArrowDropDownIcon className="icon" />
 						<div className="options">
 							<span>Settings</span>
 							<span>Logout</span>
